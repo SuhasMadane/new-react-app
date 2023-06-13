db6977566f22ef680f8193c7c63edbcf3cfe37a1
Suhas Madane <madanesuhas9@gmail.com>
2023-06-13 22:41:21 +0530

    curly braces

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ function sum(){
   let name = "Suhas"
   //multiple tags should be enclosed in one parent tag
   //if you writing statements in multlines then paranthesis is mendattory,if single statement then parenthesis is optional
-  return <>
+  return (
+      <>
          <h1>Hello WWorld</h1>
          <h1>{srt}</h1>
          <h1>pi:{pi}</h1>
-        </>
+      </>
+  ); 
 }
 export default sum;
