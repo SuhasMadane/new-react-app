b6820d1a55c893cbc7d6461be2690ca80d952c54
Suhas Madane <madanesuhas9@gmail.com>
2023-06-14 01:11:28 +0530

    style

--- src/App.js
+++ src/App.js
@@ -2,19 +2,11 @@ function sum(){
  let style = {background:"blue",color:"orange"}
 
  return (
-      <>
-      <h4>Good Night</h4>
-      <h4>{rst}</h4>
-      <h4>10</h4>
-      <h4>{value}</h4>
-      {condn ? <h4>Good Night</h4> : <h4>Good Morning</h4>}
-      <h4>{pi}</h4>
-      <h4> ID: {user.id} , Name: {user.name}</h4>
-      <h1 id="id1">Hello World</h1>
-      <h1 id={"id1"}>Hello World</h1>
-      <h1 id={`id1`}>Hello World</h1>
-      <h1 id={id}>Hello World</h1>
-      </>
+      <span>
+      <h4 style={style}>Good Night</h4>
+      <h3 style={{backgroundColor:"green",color:"black"}}>Good Morning</h3>
+      
+      </span>
   )
 }
 export default sum;
