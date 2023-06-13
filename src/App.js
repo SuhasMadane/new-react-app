e2e202badaeddc12849586d9a91ecd1a1bbba928
Suhas Madane <madanesuhas9@gmail.com>
2023-06-13 22:36:17 +0530

    js in html

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 function sum(){
-  
+  let srt = "Hello Everyone";
+  let pi = 3.142;
+  let name = "Suhas"
   return <>
          <h1>Hello WWorld</h1>
-        
+         <h1>{srt}</h1>
+         <h1>pi:{pi}</h1>
         </>
 }
 export default sum;
