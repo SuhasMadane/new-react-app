18315516df8301462b1018dc5893270fdf8e594b
Suhas Madane <madanesuhas9@gmail.com>
2023-06-13 22:39:42 +0530

    jsx rules & syntax

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ function sum(){
   let srt = "Hello Everyone";
   let pi = 3.142;
   let name = "Suhas"
+  //multiple tags should be enclosed in one parent tag
+  //if you writing statements in multlines then paranthesis is mendattory,if single statement then parenthesis is optional
   return <>
          <h1>Hello WWorld</h1>
          <h1>{srt}</h1>
