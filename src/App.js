545aa6344b70c89620e6b80b10a725fc8dcd3dc5
Suhas Madane <madanesuhas9@gmail.com>
2023-06-14 01:15:54 +0530

    external-style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import "./App.css";
 function sum(){
  let style = {background:"blue",color:"orange"}
 
  return (
       <span>
-      <h4 style={style}>Good Night</h4>
+      <h4 className="external">Good Night</h4>
       <h3 style={{backgroundColor:"green",color:"black"}}>Good Morning</h3>
       
       </span>
